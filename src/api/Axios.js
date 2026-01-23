284a9b640cdd1c99f43fcf2ffa99c7cd5f7ab4b0
Krisnaadiguna <krisnaaddd026@gmail.com>
2026-01-23 09:36:36 +0800

    feat: update API base URL to production endpoint

--- src/api/Axios.js
+++ src/api/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8000/api', 
+  baseURL: 'https://kodyakostapi.adityavisual.my.id/api', 
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
